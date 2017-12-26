7da13b16bc5c98aeae314fb7c95b0e20b45f9207
rahulb11 <rahulbhandari11@gmail.com>
2017-12-26 15:36:56 -0600

    Added a click event

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,29 +2,32 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export class Home extends React.Component {
+
+    constructor(props) {
+        super();
+        this.age = props.age;
+    }
+
+    makeMeOlder() {
+        this.age += 3;
+        console.log(this.age);
+    }
+
     render() {
-        var text = "Something";
         return (
             <div>
                 <p>In a new component</p>
-                <p>{text}</p>
-                <p>Your name is {this.props.name}, your age is {this.props.age}</p>
-                <p>User Object => Name: {this.props.user.name}</p>
-                <div>
-                    <h4>Hobbies</h4>
-                    <ul>
-                        {this.props.user.hobbies.map((hobby,i) => <li key={i}>{hobby}</li>)}
-                    </ul>
-                </div>
-                <br/>
-                {this.props.children}
+                <p>Your name is {this.props.name}, your age is {this.age}</p>
+                <hr/>
+               {/*<button onClick={this.makeMeOlder.bind(this)} className="btn btn-primary">*/}
+               <button onClick={() => this.makeMeOlder()} className="btn btn-primary">
+                    Make me older!
+                </button>
             </div>
         );
     }
 }
 Home.propTypes = {
     name: PropTypes.string,
-    age: PropTypes.number,
-    user: PropTypes.object,
-    children: PropTypes.element.isRequired
+    age: PropTypes.number
 };
