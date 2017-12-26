2dbf2cf4b3b60d5becd6b66e70c659f996365e10
rahulb11 <rahulbhandari11@gmail.com>
2017-12-26 11:41:45 -0600

    Added props

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,11 +1,30 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export class Home extends React.Component {
     render() {
+        var text = "Something";
         return (
             <div>
                 <p>In a new component</p>
+                <p>{text}</p>
+                <p>Your name is {this.props.name}, your age is {this.props.age}</p>
+                <p>User Object => Name: {this.props.user.name}</p>
+                <div>
+                    <h4>Hobbies</h4>
+                    <ul>
+                        {this.props.user.hobbies.map((hobby,i) => <li key={i}>{hobby}</li>)}
+                    </ul>
+                </div>
+                <br/>
+                {this.props.children}
             </div>
         );
     }
 }
+Home.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    user: PropTypes.object,
+    children: PropTypes.element.isRequired
+};
